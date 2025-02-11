fc9f1e5ca07204b5df7f16c9d725ecdbec6e961c
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2025-02-11 10:47:39 +0300

    Implement api call functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps } from "next"
+import Link from "next/link"
 import { Geist, Geist_Mono } from "next/font/google"
 
 const geistSans = Geist({
@@ -10,6 +12,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 })
 
-export default function Home() {
-  return <div className={`${geistSans.variable} ${geistMono.variable} `}></div>
+type dataItems = {
+  id: number
+  name: string
+}
+
+interface dataProps {
+  data: dataItems[]
+}
+
+export default function Home({ data }: dataProps) {
+  return (
+    <div className={`${geistSans.variable} ${geistMono.variable} mt-8 `}>
+      {data.map((item) => (
+        <p key={item.id}>
+          <Link href={`/productDetail/${item.id}`}>
+            {item.id} - {item.name}
+          </Link>
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/comments")
+  const data = await res.json()
+
+  return { props: { data }, revalidate: 5 }
 }
--- pages/productDetail/[id].tsx
+++ pages/productDetail/[id].tsx
@@ -1,7 +1,22 @@
 import React from "react"
+import { GetServerSideProps } from "next"
 
-const ProductDetail = () => {
-  return <div></div>
+type dataItems = {
+  id: number
+  name: string
+}
+
+const ProductDetail = ({ data }: { data: dataItems }) => {
+  return <div>{data?.name}</div>
 }
 
 export default ProductDetail
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/comments/${context.params?.id}`
+  )
+  const data = await res.json()
+
+  return { props: { data } }
+}
